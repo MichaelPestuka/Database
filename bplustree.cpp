8bcf0d062d75f74491ec1935cf5ca1dafa6bc9a5
MichaelPestuka <contact@michaelpestuka.cz>
2025-10-07 15:17:24 +0200

    File interface

--- bplustree.cpp
+++ bplustree.cpp
@@ -1,8 +1,14 @@
 #include "bplustree.hpp"
 #include <algorithm>
 #include <cstdint>
+#include <fcntl.h>
+#include <fstream>
 #include <functional>
+#include <ios>
 #include <iostream>
+#include <iterator>
+#include <sys/mman.h>
+#include <unistd.h>
 #include <vector>
 
 BPlusNode::BPlusNode(BNodeType type) {
@@ -86,8 +92,92 @@ BPlusNode BPlusNode::DeleteKV(vector<uint8_t> key) {
     return *this;
 }
 
+vector<uint8_t> BPlusNode::Serialize() {
+    vector<uint8_t> serialized;
+    serialized.reserve(4096);
+
+    serialized.push_back(type);
+    serialized.push_back(keys.size() >> 8);
+    serialized.push_back(keys.size());
+    // Key offsets
+    uint16_t cumulative_offset = 0;
+    for(auto key : keys) {
+        serialized.push_back(cumulative_offset >> 8);
+        serialized.push_back(cumulative_offset);
+        cumulative_offset += key.size();
+    }
+    serialized.push_back(cumulative_offset >> 8);
+    serialized.push_back(cumulative_offset);
+    // Value offsets
+    if(type == BNodeType::NODE) {
+        for(int i = 0; i < keys.size() + 1; i++) {
+            serialized.push_back(cumulative_offset >> 8);
+            serialized.push_back(cumulative_offset);
+            cumulative_offset += 4;
+        }
+        serialized.push_back(cumulative_offset >> 8);
+        serialized.push_back(cumulative_offset);
+    }
+    else {
+        for(auto value : values) {
+            serialized.push_back(cumulative_offset >> 8);
+            serialized.push_back(cumulative_offset);
+            cumulative_offset += value.size();
+        }
+        serialized.push_back(cumulative_offset >> 8);
+        serialized.push_back(cumulative_offset);
+    }
+    for(auto key : keys) {
+        serialized.insert(serialized.end(), key.begin(), key.end());
+    }
+    if(type == BNodeType::NODE) {
+        for(auto ptr : pointers) {
+            serialized.push_back(ptr >> 24);
+            serialized.push_back(ptr >> 16);
+            serialized.push_back(ptr >> 8);
+            serialized.push_back(ptr);
+        }
+    }
+    else {
+        for(auto value: values) {
+            serialized.insert(serialized.end(), value.begin(), value.end());
+        }
+    }
+    // serialized.resize(4096);
+
+    return serialized;
+}
+
+// Deserialize constructor
+BPlusNode::BPlusNode(vector<uint8_t> data) {
+    type = static_cast<BNodeType>(data[0]);
+
+    uint16_t key_count = 0;
+    key_count += data[1] << 8;
+    key_count += data[2];
+
+    uint16_t keys_start = 3 + (key_count + 1) * 4;
+    for(int i = 0; i < key_count * 2; i += 2) {
+        uint16_t start_offset = (data[i + 3] << 8) + data[i + 4];
+        uint16_t end_offset = (data[i + 5] << 8) + data[i + 6];
+        vector<uint8_t> key;
+        key.reserve(end_offset - start_offset);
+        std::copy(data.begin() + keys_start + start_offset, data.begin() + keys_start + end_offset, std::back_inserter(key));
+        keys.push_back(key);
+    }
+    uint16_t values_offsets = 3 + (key_count + 1) * 2;
+    for(int i = 0; i < key_count * 2; i += 2) {
+        uint16_t start_offset = (data[i + values_offsets] << 8) + data[i + values_offsets + 1];
+        uint16_t end_offset = (data[i + values_offsets + 2] << 8) + data[i + values_offsets + 3];
+        vector<uint8_t> value;
+        value.reserve(end_offset - start_offset);
+        std::copy(data.begin() + keys_start + start_offset, data.begin() + keys_start + end_offset, std::back_inserter(value));
+        values.push_back(value);
+    }
+}
+
 void BPlusNode::PrintNodeData() {
-    std::cout << "Node type: " << type << "\n";
+    std::cout << "Node type: " << (uint)type << "\n";
     std::cout << "KVs: \n";
     for(int i = 0; i < keys.size(); i++) {
         std::cout << "Key: ";
@@ -101,4 +191,58 @@ void BPlusNode::PrintNodeData() {
         std::cout << "\n";
     }
     std::cout << std::endl;
+}
+
+
+BPlusTree::BPlusTree(std::string filename) {
+    this->filename = filename;
+
+    file_page_count = 16;
+
+    int fd = open(filename.c_str(), O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    ftruncate(fd, 4096 * file_page_count); // TODO dynamic size
+    file_start_pointer = static_cast<uint8_t*>(mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
+    close(fd);
+}
+
+// Disk manager
+
+DiskManager::DiskManager(std::string filename) {
+    file_descriptor = open(filename.c_str(), O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    page_count = 0;
+    SetFilePageCount(8);
+}
+
+DiskManager::~DiskManager() {
+    close(file_descriptor);
+}
+
+void DiskManager::SetFilePageCount(uint64_t n_pages) {
+    if(n_pages > page_count) {
+        ftruncate(file_descriptor, n_pages * 4096);
+        for(int i = page_count; i < page_count + n_pages; i++) {
+            auto new_mapping = static_cast<uint8_t*>(mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_SHARED, file_descriptor, i * 4096));
+            free_pages.push_back(new_mapping);
+        }
+        
+    }
+    else {
+        // TODO
+    }
+    page_count = n_pages;
+}
+
+uint8_t* DiskManager::GetFreePage() {
+    if(free_pages.size() < 5) {
+        SetFilePageCount(page_count * 2);
+    }
+    auto page = free_pages.front();
+    free_pages.pop_front();
+    return page;
+}
+
+void DiskManager::WriteNode(BPlusNode node) {
+    auto page = GetFreePage();
+    auto node_data = node.Serialize();
+    std::copy(node_data.begin(), node_data.end(), page);
 }
--- bplustree.hpp
+++ bplustree.hpp
@@ -1,6 +1,9 @@
 
 #include <cstdint>
+#include <deque>
+#include <fstream>
 #include <vector>
+#include <string>
 
 using std::vector;
 
@@ -11,9 +14,11 @@ enum BNodeType : uint8_t {
     LEAF
 };
 
+// Handles indvidual node operations
 class BPlusNode {
     public:
         BPlusNode(BNodeType type);
+        BPlusNode(vector<uint8_t> data);
         uint16_t GetBytes();
 
 
@@ -25,6 +30,8 @@ class BPlusNode {
         BPlusNode DeleteKV(vector<uint8_t> key);
         BPlusNode UpdateKV(vector<uint8_t> key, vector<uint8_t> value);
 
+        vector<uint8_t> Serialize();
+
         void PrintNodeData();
     
     private:
@@ -34,5 +41,42 @@ class BPlusNode {
         vector<vector<uint8_t>> keys;
         vector<uint64_t> pointers;
         vector<vector<uint8_t>> values;            
+
+};
+
+// Handles Insert, Updata, Delete operations
+class BPlusTree {
+    public:
+        BPlusTree(std::string filename);
+
+        void Insert(vector<uint8_t> key, vector<uint8_t> value);
+        void Update(vector<uint8_t> key, vector<uint8_t> value);
+        void Delete(vector<uint8_t> key);
+
+        vector<uint8_t> Get(vector<uint8_t> key);
+
+    private:
+        std::string filename;
+        uint64_t root_pointer;
+        uint8_t* file_start_pointer;
+        uint64_t file_page_count;
+
+};
+
+// Handles IO
+class DiskManager {
+    public:
+        DiskManager(std::string filename);
+        ~DiskManager();
+        uint8_t* GetRoot();
+        BPlusNode GetNode();
+        uint8_t* GetFreePage();
+        void WriteNode(BPlusNode node);
+    private:
+        void SetFilePageCount(uint64_t n_pages);
+        int file_descriptor;
+        uint64_t root;
+        uint64_t page_count;
+        std::deque<uint8_t*> free_pages;
 
 };
